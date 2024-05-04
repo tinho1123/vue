b0a75657e58627558478528c909a87ee73e84d4a
Wellington Carvalho da Cunha Filho <Wellington.carvalho@CDC.TITAN>
2024-05-04 18:19:24 -0300

    vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script>
+    import {useCounterStore} from '@/stores/counter';
+    const counter = useCounterStore();
+    counter.increment();
+
+</script>
+
+<template>
+    <h1>    
+        {{counter.count}}
+    </h1>
+</template>
